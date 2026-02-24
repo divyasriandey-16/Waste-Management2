dfa8daa1f1d401bf56afb7380f925d0aa28361f3
Lakshmi-durga-03 <nagalakshmienugu@gmail.com>
2026-02-24 22:30:32 +0530

    Initial commit - Smart Food Share

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,54 +1,65 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
+import "./login.css";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
   const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
 
-    const res = await axios.post("http://localhost:5000/api/login", {
-      email,
-      password
-    });
+    try {
+      const res = await axios.post("http://localhost:5000/api/login", {
+        email,
+        password
+      });
 
-    localStorage.setItem("token", res.data.token);
-localStorage.setItem("user", JSON.stringify(res.data.user));
+      localStorage.setItem("token", res.data.token);
+      localStorage.setItem("user", JSON.stringify(res.data.user));
 
-    alert("Login successful");
-    navigate("/dashboard");
+      navigate("/dashboard");
+    } catch (err) {
+      alert("Invalid login");
+    }
   };
 
   return (
-    <div style={{ padding: "20px" }}>
+  <div className="login-wrapper">
+    <div className="login-card">
+      <h1 className="brand">SmartShare</h1>
+      <p className="tagline">
+  🍱 Share Food. <span style={{color:"#fc8019"}}>Save Lives.</span>
+</p>
+
       <h2>Login</h2>
+
       <form onSubmit={handleLogin}>
         <input
           type="email"
-          placeholder="Email"
+          placeholder="Enter email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-        <br /><br />
+
         <input
           type="password"
-          placeholder="Password"
+          placeholder="Enter password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <br /><br />
+
         <button type="submit">Login</button>
       </form>
 
-      <p>
+      <p className="register-text">
         Don’t have an account? <Link to="/register">Register</Link>
       </p>
     </div>
-  );
+  </div>
+);
 }
 
 export default Login;
